04a473c810b1e781265d7d6d4a44672c27b6ae60
arroychaan <achmadroychan501@gmail.com>
2026-07-25 01:58:35 +0700

    fix: use Arial Bold font (universal), Outline=6/Shadow=4 max contrast, MarginV=420 safe zone, WrapStyle=0

--- core/audio_processor.py
+++ core/audio_processor.py
@@ -329,10 +329,11 @@ def generate_ass_subtitle_file(
 ScriptType: v4.00+
 PlayResX: 1080
 PlayResY: 1920
+WrapStyle: 0
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: TikTokFYP, Liberation Sans, 84, &H00FFFFFF, &H003BEEFF, &H00000000, &H80000000, -1, 0, 0, 0, 100, 100, 0, 0, 1, 5, 3, 2, 40, 40, 480, 1
+Style: TikTokFYP, Arial, 78, &H00FFFFFF, &H003BEEFF, &H00000000, &H96000000, -1, 0, 0, 0, 100, 100, 2, 0, 1, 6, 4, 2, 40, 40, 420, 1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
